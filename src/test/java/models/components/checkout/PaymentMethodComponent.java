80d8d5c3f358bdf0224cd6a0f78094dc0a96c479
LamTruc <truclam8398@gmail.com>
2022-12-19 22:50:55 +0700

    add unnecessary folder to gitignore

--- src/test/java/models/components/checkout/PaymentMethodComponent.java
+++ src/test/java/models/components/checkout/PaymentMethodComponent.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 
 @ComponentCssSelector(value = "#opc-payment_method")
 public class PaymentMethodComponent extends Component {
+
     public PaymentMethodComponent(WebDriver driver, WebElement component) {
         super(driver, component);
     }
