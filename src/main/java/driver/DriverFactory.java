bd0c09ac4bc484b41940b5b72cad8d2e33f5e284
LamTruc <truclam8398@gmail.com>
2023-01-29 22:26:53 +0700

    handle remote running

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,12 +1,15 @@
 package driver;
 
-import org.apache.commons.exec.OS;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.net.URL;
 import java.time.Duration;
 
 public class DriverFactory {
@@ -27,7 +30,14 @@ public class DriverFactory {
     }
 
     public WebDriver getDriver(String browserName) {
-        if (driver == null) {
+        boolean isRemoteRunning = System.getProperty("hub") != null;
+        if(isRemoteRunning) return getRemoteWebDriver(browserName, System.getProperty("hub"));
+        else return getLocalDriver(browserName);
+
+    }
+
+    private WebDriver getLocalDriver(String browserName) {
+        if(driver == null) {
             switch (browserName) {
                 case ("chrome"):
                     driver = new ChromeDriver();
@@ -40,6 +50,35 @@ public class DriverFactory {
                     break;
                 default:
                     throw new IllegalArgumentException(browserName + " is not supported!");
+            }
+        }
+        return driver;
+    }
+
+    private WebDriver getRemoteWebDriver(String browserName, String hub) {
+        if(driver == null) {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setPlatform(Platform.ANY);
+            switch (browserName) {
+                case ("chrome"):
+                    desiredCapabilities.setBrowserName("chrome");
+                    break;
+                case "firefox":
+                    desiredCapabilities.setBrowserName("firefox");
+                    break;
+                case "safari":
+                    desiredCapabilities.setBrowserName("safari");
+                    break;
+                default:
+                    throw new IllegalArgumentException(browserName + " is not supported!");
+            }
+
+            try {
+                URL hubUrl = new URL(hub.concat("/wd/hub"));
+                driver = new RemoteWebDriver(hubUrl, desiredCapabilities);
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15L));
+            } catch (Exception e){
+                e.printStackTrace();
             }
         }
         return driver;
